c75b2c04b8aba9bbfca5aaaf966f179ed51f362f
Nari Kang <narikang333@gmail.com>
2024-03-08 22:01:18 -0700

    changed function name

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import schoolData from './CollegeBasketballTeams.json';
-import { StringLiteral } from 'typescript';
 
 interface Team {
   school: string;
